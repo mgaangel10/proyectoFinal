acfa87cb9999ebdefd7b2a58cf234b79092a37e5
miguel angel <mg.aangel10@gmail.com>
2023-05-11 09:19:33 +0200

    add empleados

---
+++ ProyectoFinal-1/src/main/java/com/salesianostriana/dam/ProyectoFinalPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalPruebaApplication.class, args);
+	}
+
+}
